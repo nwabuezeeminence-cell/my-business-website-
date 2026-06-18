93d7dfcc541d46b822036691a5abecce0cffb62e
nwabuezeeminence-cell <nwabuezeeminence@gmail.com>
2026-06-18 11:44:01 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener("install", function(e){
+  console.log("NEXA PWA Installed");
+});
+
+self.addEventListener("fetch", function(e){
+  // basic offline support
+});
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("nexa-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/about.html",
+        "/services.html",
+        "/contact.html",
+        "/css/style.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
