247771b803e079928401d815e4136a19a9122a5a
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-06-22 17:24:16 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,3 @@
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-database-compat.js"></script>
 const db = firebase.database();
 
 // ========================
