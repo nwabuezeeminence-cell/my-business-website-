36974416cffa110d939f973c5664380ecff5609a
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-06-27 09:09:54 +0100

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import {
+    getDatabase,
+    ref,
+    push,
+    set,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
