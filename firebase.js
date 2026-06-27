8c9f245d86c19b688ef920814b6b1b07281e8f04
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-06-27 11:44:12 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "nexa-digital-services.firebaseapp.com",
+    databaseURL: "https://nexa-digital-services-default-rtdb.firebaseio.com",
+    projectId: "nexa-digital-services",
+    storageBucket: "nexa-digital-services.firebasestorage.app",
+    messagingSenderId: "552141783731",
+    appId: "1:552141783731:web:addf613eba5281388766c6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Services
+const db = firebase.database();
