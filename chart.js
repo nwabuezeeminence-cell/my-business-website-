a667663a75675fa6da0b8a066fd4e47aa11ee2c7
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-06-27 09:10:29 +0100

    Update chart.js

--- chart.js
+++ chart.js
@@ -11,3 +11,8 @@ import {
 import {
     getAuth
 } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+const auth = getAuth(app);
