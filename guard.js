7be534aabda9edf038af4e9661226d8267d8394a
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-06-29 07:34:10 +0100

    Update guard.js

--- guard.js
+++ guard.js
@@ -2,6 +2,12 @@ firebase.auth().onAuthStateChanged((user) => {
 
     if (!user) {
         window.location.href = "login.html";
+"community.html";
+"chat.html";
+"group.html";
+"profile.html";
+"settings.html";
+"admin.html";
         return;
     }
 
