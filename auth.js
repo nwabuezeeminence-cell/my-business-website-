6ab7b511e00788be3cd22e9aa6e72078513acfd4
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-06-29 07:15:35 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -34,7 +34,13 @@ function signUp(name, email, password) {
 function login(email, password) {
 
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
+    .then(() => {
+
+        const user = firebase.auth().currentUser;
+
+        db.ref("users/" + user.uid).update({
+            online: true
+        });
 
         alert("Login successful!");
 
