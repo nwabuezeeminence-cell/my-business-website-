7514b857f463e5cb7e109d47361819af7fd60781
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-07-13 03:10:15 +0100

    Update attachment.js

--- attachment.js
+++ attachment.js
@@ -4,3 +4,20 @@ const galleryBtn = document.getElementById("galleryBtn");
 galleryBtn.onclick = () => {
     imagePicker.click();
 };
+imagePicker.onchange = (e) => {
+
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+
+        showImagePreview(file, reader.result);
+
+    };
+
+    reader.readAsDataURL(file);
+
+};
