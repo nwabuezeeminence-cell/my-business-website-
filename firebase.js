25836383ad5e12dd0ba48a11da36bc7049622e0d
Nwabueze Eminence <nwabuezeeminence@gmail.com>
2026-08-06 13:15:05 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,15 @@
-// Firebase Configuration
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDluVqK67XEYtc4f9rkML2diChbSvgiYiU",
-    authDomain: "nexa-digital-services.firebaseapp.com",
-    databaseURL: "https://nexa-digital-services-default-rtdb.firebaseio.com",
-    projectId: "nexa-digital-services",
-    storageBucket: "nexa-digital-services.firebasestorage.app",
-    messagingSenderId: "552141783731",
-    appId: "1:552141783731:web:addf613eba5281388766c6"
+  apiKey: "AIzaSyDluVqK67XEYtc4f9rkML2diChbSvgiYiU",
+  authDomain: "nexa-digital-services.firebaseapp.com",
+  databaseURL: "https://nexa-digital-services-default-rtdb.firebaseio.com",
+  projectId: "nexa-digital-services",
+  storageBucket: "nexa-digital-services.firebasestorage.app",
+  messagingSenderId: "552141783731",
+  appId: "1:552141783731:web:addf613eba5281388766c6",
+  measurementId: "G-EN9MCL80EH"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Services
-const db = firebase.database();
 const auth = firebase.auth();
-
-
-
-
-
+const db = firebase.database();
+// const analytics = firebase.analytics();  <-- DELETE THIS LINE
